afb085148d985f2f74153672ece9f52e342af6cc
layth <jaber.layth@gmail.com>
2022-07-23 16:43:53 +0100

    change project name

--- gsm-guide-backoffice/src/app/app.component.spec.ts
+++ gsm-guide-backoffice/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coucou-backoffice'`, () => {
+  it(`should have as title 'gsm-backoffice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('coucou-backoffice');
--- gsm-guide-backoffice/src/app/app.component.ts
+++ gsm-guide-backoffice/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'coucou-backoffice';
+  title = 'gsm-backoffice';
 
   ngOnInit(): void {
   }
